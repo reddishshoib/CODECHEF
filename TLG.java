be36b0c04c4d7c396af9ff3ecdaf5725c28b94ec
reddishshoib <39012097+reddishshoib@users.noreply.github.com>
2021-05-01 09:41:37 +0000

    Update TLG.java

--- TLG.java
+++ TLG.java
@@ -2,28 +2,27 @@ import java.util.*;
 public class TLG
 {
 	public static void main(String[] args) {
-		int n,p1,p2,w=0,d=0,wn=0,wd=0;
+		int n,p1=0,p2=0,wn=0,wd=0;
 		Scanner sc= new Scanner(System.in);
 		n = sc.nextInt();
 		for (int i=0; i<n ; i++ ) 
 		{
-			p1=sc.nextInt();
-			p2=sc.nextInt();
-			if(p1>p2)
+			int s=sc.nextInt();
+			int l=sc.nextInt();
+			p1=p1+s;
+			p2=p2+l;
+
+			if(wd<p1-p2 && p1>p2)
 			{
-				w=1;
-				d=p1-p2;	
+				wn=1;
+				wd=p1-p2;	
 			}
-			else 
+			else if(wd<p2-p1 && p2>p1)
 			{
-				w=2;
-				d=p2-p1;
+				wn=2;
+				wd=p2-p1;
 			}
-			if(d>wd)
-			{
-				wn=w;
-				wd=d;
-			}
+			
 
 		}
 		System.out.println(wn + " "+wd);
